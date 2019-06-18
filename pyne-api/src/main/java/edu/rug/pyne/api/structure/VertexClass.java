62a7a586e35a55418a204a042cafc3de70428801
Patrick Beuks (killje) <code@beuks.net>
2019-06-18 18:06:32 +0200

    Added java documentation to VertexClass and VertexPackage

--- pyne-api/src/main/java/edu/rug/pyne/api/structure/VertexClass.java
+++ pyne-api/src/main/java/edu/rug/pyne/api/structure/VertexClass.java
@@ -16,31 +16,66 @@ import org.apache.tinkerpop.gremlin.structure.T;
 import spoon.reflect.declaration.CtType;
 
 /**
+ * This is a structure for a TinkerPop vertex.
  *
+ * This vertex is meant to represent a class in the dependency graph.
  *
  * @author Patrick Beuks (s2288842) <code@beuks.net>
  */
 @GraphElement
 public abstract class VertexClass extends AbstractVertexFrame {
 
+    /**
+     * The label for this vertex
+     */
     public static final String LABEL = "class";
 
+    /**
+     * The class types possible.
+     *
+     * A system class represents a class created from source file.
+     *
+     * A retrieved class represents a class that is outside the source files
+     */
     public enum ClassType {
         SystemClass,
         RetrievedClass;
     }
 
-    public static VertexClass createRetrievedClass(FramedGraph framedGraph, String clazz) {
+    /**
+     * This static function creates a new retrieved class from a given name
+     *
+     * @param framedGraph The graph to add the class vertex to
+     * @param clazz The name the class vertex should have
+     * @return The newly created class vertex
+     */
+    public static VertexClass createRetrievedClass(
+            FramedGraph framedGraph, String clazz
+    ) {
 
-        VertexClass vertex = framedGraph.addFramedVertex(new DefaultClassInitializer<>(VertexClass.class), T.label, LABEL);
+        // Create the vertex class with the class label
+        VertexClass vertex = framedGraph.addFramedVertex(
+                new DefaultClassInitializer<>(VertexClass.class), T.label, LABEL
+        );
+
         vertex.setName(clazz);
         vertex.setClassType(ClassType.RetrievedClass);
+        // Not implemented for retieved classes
         vertex.setClassModifier("none");
 
         return vertex;
     }
 
-    public static VertexClass createSystemClass(FramedGraph framedGraph, CtType clazz) {
+    /**
+     * This static function creates a new system class from a CtTypr
+     *
+     * @param framedGraph The graph to add the class vertex to
+     * @param clazz The class this class vertex is created for
+     * @return The newly created class vertex
+     */
+    public static VertexClass createSystemClass(
+            FramedGraph framedGraph, CtType clazz
+    ) {
 
         String classModifier = "none";
 
@@ -51,7 +86,11 @@ public abstract class VertexClass extends AbstractVertexFrame {
             classModifier = "Interface";
         }
 
-        VertexClass vertex = framedGraph.addFramedVertex(new DefaultClassInitializer<>(VertexClass.class), T.label, LABEL);
+        // Create the vertex class with the class label
+        VertexClass vertex = framedGraph.addFramedVertex(
+                new DefaultClassInitializer<>(VertexClass.class), T.label, LABEL
+        );
+
         vertex.setName(clazz.getQualifiedName());
         vertex.setClassType(ClassType.SystemClass);
         vertex.setClassModifier(classModifier);
@@ -59,26 +98,96 @@ public abstract class VertexClass extends AbstractVertexFrame {
         return vertex;
     }
 
-    public static VertexClass getVertexClassByName(FramedGraph framedGraph, String name) {
+    /**
+     * This static function gives a vertex class back from a qualified class
+     * name or returns null when not found.
+     *
+     * @param framedGraph The graph to find the vertex class on
+     * @param name The qualified name of the class
+     * @return The vertex class, or null if not found
+     */
+    public static VertexClass getVertexClassByName(
+            FramedGraph framedGraph, String name
+    ) {
+
+        // Find the class vertexes and then find the name
         return framedGraph.traverse(
                 (g) -> g.V().hasLabel(LABEL).has("name", name)
         ).nextOrDefault(VertexClass.class, null);
     }
 
+    /**
+     * Returns the name of the vertex class
+     *
+     * @return The name property
+     */
     @Property("name")
     public abstract String getName();
 
+    /**
+     * Sets the name property on the vertex
+     *
+     * @param name The name to set
+     */
     @Property("name")
     public abstract void setName(String name);
-    
+
+    /**
+     * This function returns the class type of the vertex class
+     *
+     * @return The class type
+     */
+    public ClassType getClassType() {
+        return ClassType.valueOf(getProperty("ClassType", String.class));
+    }
+
+    /**
+     * This function sets the class type of the vertex class
+     *
+     * @param classType The class type to set
+     */
+    public void setClassType(ClassType classType) {
+        setProperty("ClassType", classType.name());
+    }
+
+    /**
+     * Returns true is the property class type equals to ClassType.SystemClass
+     *
+     * @return True if it is a system class, false otherwise
+     */
+    public boolean isSystemClass() {
+        return getClassType().equals(ClassType.SystemClass);
+    }
+
+    /**
+     * Gets the class modifier property for this vertex class
+     *
+     * @return The class modifier
+     */
+    @Property("classModifier")
+    public abstract String getClassModifier();
+
+    /**
+     * Sets the class modifier property on the vertex class
+     *
+     * @param classModifier The class modifier to set
+     */
+    @Property("classModifier")
+    public abstract void setClassModifier(String classModifier);
+
+    /**
+     * This function removes all the edges from the vertex class
+     */
     public void removeEdges() {
-        
         removeDependsOn();
         removeImplementationOf();
         removeChildsOf();
-        
     }
-    
+
+    /**
+     * This function overrides the default remove, also deleting all edges
+     * and belonging edges as well as the node.
+     */
     @Override
     public void remove() {
         removeEdges();
@@ -86,62 +195,99 @@ public abstract class VertexClass extends AbstractVertexFrame {
         super.remove();
     }
 
-    public ClassType getClassType() {
-        return ClassType.valueOf(getProperty("ClassType", String.class));
-    }
-
-    public void setClassType(ClassType classType) {
-        setProperty("ClassType", classType.name());
-    }
-
-    public boolean isSystemClass() {
-        return getClassType().equals(ClassType.SystemClass);
-    }
-
-    @Property("classModifier")
-    public abstract String getClassModifier();
-
-    @Property("classModifier")
-    public abstract void setClassModifier(String classModifier);
-
+    /**
+     * Returns the edge for belongs to. In a valid graph only one per vertex
+     * should exists
+     *
+     * @return The belongsTo edge
+     */
     @Incidence(label = "belongsTo")
     public abstract EdgeBelongsTo getBelongsTo();
 
+    /**
+     * Returns the VertexPackage the vertex class belongs to. In a valid graph
+     * only one per vertex should exists
+     *
+     * @return The VertexPackage where the out of the belongsTo edge points to
+     */
     @Adjacency(label = "belongsTo")
     public abstract VertexPackage getBelongsToPackage();
 
+    /**
+     * This sets the vertex package this vertex class belongs to.
+     *
+     * Only one per vertex class can exists. If a previous edge exists, that
+     * edge is removed and a new one is set.
+     *
+     * This function also increases the total number of classes the package has.
+     *
+     * @param vertexPackage The vertex package this vertex class belongs to
+     * @return The newly created edge
+     */
     public EdgeBelongsTo setBelongsTo(VertexPackage vertexPackage) {
+        // Get previous set edges for belongs to
         Iterator<Edge> edges = getElement().edges(Direction.OUT, "belongsTo");
+        // Remove all found edges if any
         if (edges.hasNext()) {
             getBelongsToPackage().decrementNumOfClassesInPackage();
             edges.next().remove();
         }
+        // This vertex class now belongs to the package, so increment the number
+        // of classes
         vertexPackage.incrementNumOfClassesInPackage();
+
         return addFramedEdge("belongsTo", vertexPackage, EdgeBelongsTo.class);
     }
 
+    /**
+     * This function removes the belongsTo edge. This also decrements the total
+     * number of the package this class belonged to.
+     */
     public void removeBelongsTo() {
-        if (getBelongsToPackage() == null) {
-            System.out.println(getName());
-        }
-        getBelongsToPackage().decrementNumOfClassesInPackage();
-        if (getBelongsToPackage().getNumOfClassesInPackage() == 0) {
-            getBelongsToPackage().remove();
-        } else {
-            getBelongsTo().remove();
+        VertexPackage belongsToPackage = getBelongsToPackage();
+
+        belongsToPackage.decrementNumOfClassesInPackage();
+        getBelongsTo().remove();
+        
+        // If no more classes belong to the package, remove the package.
+        if (belongsToPackage.getNumOfClassesInPackage() == 0) {
+            belongsToPackage.remove();
         }
     }
-    
+
+    /**
+     * Gets a list of edges that this vertex class is afferent of.
+     *
+     * @return The list of afferent edges
+     */
     @Incidence(label = "isAfferentOf")
     public abstract List<EdgeIsAfferentOf> getAfferentOfEdges();
 
+    /**
+     * Gets a list of packages that this vertex class is afferent of.
+     *
+     * @return The list of afferent vertex packages
+     */
     @Adjacency(label = "isAfferentOf")
     public abstract List<VertexPackage> getAfferentOfPackages();
 
+    /**
+     * Adds an edge between a class and package that denotes that this vertex
+     * class is afferent of the given package.
+     *
+     * This also adds a edge between this class belongsTo package and the given
+     * package if this edge does not already exists.
+     *
+     * @param vertexPackage The vertex package this vertex class is afferent of
+     * @return The newly created edge
+     */
     public EdgeIsAfferentOf addAfferentOf(VertexPackage vertexPackage) {
-        
-        EdgeIsAfferentOf edgeIsAfferentOf = addFramedEdge("isAfferentOf", vertexPackage, EdgeIsAfferentOf.class);
 
+        EdgeIsAfferentOf edgeIsAfferentOf = addFramedEdge(
+                "isAfferentOf", vertexPackage, EdgeIsAfferentOf.class
+        );
+
+        // Check if we need to add a edge between packages.
         VertexPackage belongsToPackage = getBelongsToPackage();
         if (!belongsToPackage.getAfferentOfPackages().contains(vertexPackage)) {
             belongsToPackage.addAfferentOfPackage(vertexPackage);
@@ -150,80 +296,165 @@ public abstract class VertexClass extends AbstractVertexFrame {
         return edgeIsAfferentOf;
     }
 
+    /**
+     * Gets a list of edges that this vertex class is efferent of.
+     *
+     * @return The list of efferent edges
+     */
     @Incidence(label = "isEfferentOf")
     public abstract List<EdgeIsEfferentOf> getEfferentOfEdges();
 
+    /**
+     * Gets a list of packages that this vertex class is efferent of.
+     *
+     * @return The list of efferent vertex packages
+     */
     @Adjacency(label = "isEfferentOf")
     public abstract List<VertexPackage> getEfferentOfPackages();
 
+    /**
+     * Adds an edge between a class and package that denotes that this vertex
+     * class is efferent of the given package.
+     *
+     * @param vertexPackage The vertex package this vertex class is efferent of
+     * @return The newly created edge
+     */
     public EdgeIsEfferentOf addEfferentOf(VertexPackage vertexPackage) {
-        return addFramedEdge("isEfferentOf", vertexPackage, EdgeIsEfferentOf.class);
+        return addFramedEdge(
+                "isEfferentOf", vertexPackage, EdgeIsEfferentOf.class
+        );
     }
 
+    /**
+     * Gets a list of edges that this vertex class depends on.
+     *
+     * @return The list of dependsOn edges
+     */
     @Incidence(label = "dependsOn")
     public abstract List<EdgeDependsOn> getDependOnEdges();
 
+    /**
+     * Gets a list of classes that this vertex class depends on.
+     *
+     * @return The list of classes.
+     */
     @Adjacency(label = "dependsOn")
     public abstract List<VertexClass> getDependOnClasses();
 
+    /**
+     * This adds an edge to a class denoting that this class depends on the
+     * given class.
+     *
+     * If the edge already exists the weight of the edge is increased instead.
+     *
+     * @param dependingClass The class this vertex class depends on.
+     *
+     * @return The newly created edge, or the already existing edge that has
+     * been incremented.
+     */
     public EdgeDependsOn addDependOnClass(VertexClass dependingClass) {
-        
+
         if (dependingClass.getName().equals(getName())) {
             return null;
         }
-            
+
+        // Find if this class already depends on the given class
         Optional<EdgeDependsOn> dependency = getDependOnEdges().stream().filter(
                 (edge) -> edge.getDependOn().equals(dependingClass)
         ).findFirst();
-        
+
         EdgeDependsOn dependOnEdge;
         if (dependency.isEmpty()) {
-            dependOnEdge = addFramedEdge("dependsOn", dependingClass, EdgeDependsOn.class);
+            // Create an edge and set the weight to 1
+            dependOnEdge = addFramedEdge(
+                    "dependsOn", dependingClass, EdgeDependsOn.class
+            );
+
             dependOnEdge.setWeight(1);
         } else {
+            // Increment the weight.
             dependOnEdge = dependency.get();
             dependOnEdge.incrementWeight();
         }
         return dependOnEdge;
     }
 
+    /**
+     * Removes all depends on edges.
+     */
     @Adjacency(label = "dependsOn")
     public abstract void removeDependsOn();
-    
-    @Adjacency(label = "dependsOn", direction = Direction.IN)
-    public abstract List<VertexClass> getDependByClasses();
 
+    /**
+     * Gets a list of edges that this vertex class is a child of.
+     *
+     * @return The list of isChildOf edges
+     */
     @Incidence(label = "isChildOf")
     public abstract List<EdgeIsChildOf> getChildsOfEdges();
 
+    /**
+     * Gets a list of classes that this vertex class is a child of.
+     *
+     * @return The list of classes.
+     */
     @Adjacency(label = "isChildOf")
     public abstract List<VertexClass> getChildsOfClasses();
 
+    /**
+     * This adds a new edge between this class and the given class that denotes
+     * that this class is a child of the given class.
+     *
+     * @param childOfClass The class this class is a child of
+     * @return The newly created edge
+     */
     public EdgeIsChildOf addChildOfClass(VertexClass childOfClass) {
         return addFramedEdge("isChildOf", childOfClass, EdgeIsChildOf.class);
     }
 
+    /**
+     * Removes all is child of edges.
+     */
     @Adjacency(label = "isChildOf")
     public abstract void removeChildsOf();
 
-    @Adjacency(label = "isChildOf", direction = Direction.IN)
-    public abstract List<VertexClass> getChildsByClasses();
-
+    /**
+     * Gets a list of edges that this vertex class is an implementation of.
+     *
+     * @return The list of isImplementationOf edges
+     */
     @Incidence(label = "isImplementationOf")
     public abstract List<EdgeIsImplementationOf> getImplementationOfEdges();
 
+    /**
+     * Gets a list of classes that this vertex class is an implementation of.
+     *
+     * @return The list of classes.
+     */
     @Adjacency(label = "isImplementationOf")
     public abstract List<VertexClass> getImplementationOfClasses();
 
-    public EdgeIsImplementationOf addImplematationOfClass(VertexClass implementationOfClass) {
-        return addFramedEdge("isImplementationOf", implementationOfClass, EdgeIsImplementationOf.class);
+    /**
+     * This adds a new edge between this class and the given class that denotes
+     * that this class is an implementation of the given class.
+     *
+     * @param implementationOfClass The class this class is an implementation of
+     * @return The newly created edge
+     */
+    public EdgeIsImplementationOf addImplematationOfClass(
+            VertexClass implementationOfClass
+    ) {
+        return addFramedEdge(
+                "isImplementationOf",
+                implementationOfClass,
+                EdgeIsImplementationOf.class
+        );
     }
 
+    /**
+     * Removes all is implementation of edges.
+     */
     @Adjacency(label = "isImplementationOf")
     public abstract void removeImplementationOf();
 
-    @Adjacency(label = "isImplementationOf", direction = Direction.IN)
-    public abstract List<VertexClass> getImplementationByClasses();
-
-    
 }
--- pyne-api/src/main/java/edu/rug/pyne/api/structure/VertexPackage.java
+++ pyne-api/src/main/java/edu/rug/pyne/api/structure/VertexPackage.java
@@ -8,22 +8,44 @@ import com.syncleus.ferma.annotations.GraphElement;
 import com.syncleus.ferma.annotations.Incidence;
 import com.syncleus.ferma.annotations.Property;
 import java.util.List;
-import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.T;
 import spoon.reflect.reference.CtPackageReference;
 
 /**
+ * This is a structure for a TinkerPop vertex.
+ *
+ * This vertex is meant to represent a package in the dependency graph.
  *
  * @author Patrick Beuks (s2288842) <code@beuks.net>
  */
 @GraphElement
 public abstract class VertexPackage extends AbstractVertexFrame {
 
+    /**
+     * The label for this vertex
+     */
     public static final String LABEL = "package";
 
-    public static VertexPackage createRetrievedPackage(FramedGraph framedGraph, String PackageName) {
+    /**
+     * Creates a new vertex package with the given name with its package type
+     * set to A retrieved package, denoting a package that comes from outside
+     * the source files.
+     *
+     * @param framedGraph The graph to add the vertex to
+     * @param PackageName The name for this package
+     * @return The newly created vertex
+     */
+    public static VertexPackage createRetrievedPackage(
+            FramedGraph framedGraph, String PackageName
+    ) {
 
-        VertexPackage vertex = framedGraph.addFramedVertex(new DefaultClassInitializer<>(VertexPackage.class), T.label, LABEL);
+        // Creates a new VertexPackage with the vertex label
+        VertexPackage vertex = framedGraph.addFramedVertex(
+                new DefaultClassInitializer<>(VertexPackage.class),
+                T.label,
+                LABEL
+        );
+
         vertex.setName(PackageName);
         vertex.setPackageType("RetrievedPackage");
         vertex.setNumTotalDep(0);
@@ -32,11 +54,27 @@ public abstract class VertexPackage extends AbstractVertexFrame {
         return vertex;
     }
 
-    public static VertexPackage createVertexPackage(FramedGraph framedGraph, CtPackageReference ctPackage) {
+    /**
+     * This creates a new vertex on the graph denoting a package.
+     *
+     * @param framedGraph The graph to add the vertex to
+     * @param ctPackage The reference to the package
+     * @return The newly created vertex.
+     */
+    public static VertexPackage createVertexPackage(
+            FramedGraph framedGraph, CtPackageReference ctPackage
+    ) {
 
-        String packageType = ctPackage.isImplicit()? "RetrievedPackage" : "SystemPackage";
+        // Check if this package comes from within the source files
+        String packageType = ctPackage.isImplicit()
+                ? "RetrievedPackage" : "SystemPackage";
 
-        VertexPackage vertex = framedGraph.addFramedVertex(new DefaultClassInitializer<>(VertexPackage.class), T.label, LABEL);
+        VertexPackage vertex = framedGraph.addFramedVertex(
+                new DefaultClassInitializer<>(VertexPackage.class),
+                T.label,
+                LABEL
+        );
+
         vertex.setName(ctPackage.getQualifiedName());
         vertex.setPackageType(packageType);
         vertex.setNumTotalDep(0);
@@ -45,71 +83,186 @@ public abstract class VertexPackage extends AbstractVertexFrame {
         return vertex;
     }
 
-    public static VertexPackage getVertexPackageByName(FramedGraph framedGraph, String name) {
+    /**
+     * This static function gives a vertex package back from a qualified package
+     * name or returns null when not found.
+     *
+     * @param framedGraph The graph to find the vertex package on
+     * @param name The qualified name of the package
+     * @return The vertex package, or null if not found
+     */
+    public static VertexPackage getVertexPackageByName(
+            FramedGraph framedGraph, String name
+    ) {
+
+        // Find the package vertexes and then find the name
         return framedGraph.traverse(
                 (g) -> g.V().hasLabel(LABEL).has("name", name)
         ).nextOrDefault(VertexPackage.class, null);
     }
 
+    /**
+     * Returns the name of the vertex package
+     *
+     * @return The name property
+     */
     @Property("name")
     public abstract String getName();
 
+    /**
+     * Sets the name property on the vertex
+     *
+     * @param name The name to set
+     */
     @Property("name")
     public abstract void setName(String name);
 
+    /**
+     * Gets the PackageType property of this vertex
+     *
+     * @return The package type
+     */
     @Property("PackageType")
     public abstract String getPackageType();
 
+    /**
+     * Sets the PackageType property on this vertex.
+     *
+     * @param packageType The package type
+     */
     @Property("PackageType")
     public abstract void setPackageType(String packageType);
 
+    /**
+     * Gets the number of total dependencies this package has.
+     *
+     * @return The number of dependencies
+     */
     @Property("numTotalDep")
     public abstract int getNumTotalDep();
 
+    /**
+     * Sets the number of total dependencies this package has.
+     *
+     * @param numTotalDep The total number of dependencies to set
+     */
     @Property("numTotalDep")
     public abstract void setNumTotalDep(int numTotalDep);
 
+    /**
+     * Increment the total number of dependencies by one
+     */
     public void incrementNumTotalDep() {
         setNumTotalDep(getNumTotalDep() + 1);
     }
 
+    /**
+     * Decrement the total number of dependencies by one.
+     *
+     * @throws IllegalStateException Throws a state exception when the total
+     * number of dependencies becomes negative.
+     */
     public void decrementNumOfTotalDep() {
         int numdep = getNumTotalDep();
         if (numdep <= 0) {
-            throw new IllegalStateException("Cannot have a negative number of total depedancies");
+            throw new IllegalStateException(
+                    "Cannot have a negative number of total dependencies"
+            );
         }
         setNumTotalDep(numdep - 1);
     }
 
+    /**
+     * Gets the number of classes this package has.
+     *
+     * @return The number of classes
+     */
     @Property("numOfClassesInPackage")
     public abstract int getNumOfClassesInPackage();
 
+    /**
+     * Sets the number of classes this package has.
+     *
+     * @param numOfClassesInPackage The total number of classes to set
+     */
     @Property("numOfClassesInPackage")
     public abstract void setNumOfClassesInPackage(int numOfClassesInPackage);
 
+    /**
+     * Increment the total number of classes by one
+     */
     public void incrementNumOfClassesInPackage() {
         setNumOfClassesInPackage(getNumOfClassesInPackage() + 1);
     }
 
+    /**
+     * Decrement the total number of dependencies by one.
+     *
+     * @throws IllegalStateException Throws a state exception when the total
+     * number of classes becomes negative.
+     */
     public void decrementNumOfClassesInPackage() {
         int numcls = getNumOfClassesInPackage();
         if (numcls <= 0) {
-            throw new IllegalStateException("Cannot have a negative number of classes");
+            throw new IllegalStateException(
+                    "Cannot have a negative number of classes"
+            );
         }
         setNumOfClassesInPackage(numcls - 1);
     }
 
+    /**
+     * This function overrides the default remove, also deleting all edges as
+     * well as the node.
+     */
+    @Override
+    public void remove() {
+        removePackageIsAfferentOfEdges();
+        super.remove();
+    }
+
+    /**
+     * Gets a list of edges that this vertex package is afferent of.
+     *
+     * @return The list of afferent edges
+     */
     @Incidence(label = "packageIsAfferentOf")
     public abstract List<EdgePackageIsAfferentOf> getAfferentOfEdges();
 
+    /**
+     * Gets a list of packages that this vertex package is afferent of.
+     *
+     * @return The list of afferent vertex packages
+     */
     @Adjacency(label = "packageIsAfferentOf")
     public abstract List<VertexPackage> getAfferentOfPackages();
 
-    public EdgePackageIsAfferentOf addAfferentOfPackage(VertexPackage afferentOfPackage) {
+    /**
+     * Adds an edge between this package and package that denotes that this
+     * vertex package is afferent of the given package.
+     *
+     * @param afferentOfPackage The vertex package this vertex package is
+     * afferent of
+     * @return The newly created edge
+     */
+    public EdgePackageIsAfferentOf addAfferentOfPackage(
+            VertexPackage afferentOfPackage
+    ) {
+
         incrementNumTotalDep();
-        return addFramedEdge("packageIsAfferentOf", afferentOfPackage, EdgePackageIsAfferentOf.class);
+        return addFramedEdge(
+                "packageIsAfferentOf",
+                afferentOfPackage,
+                EdgePackageIsAfferentOf.class
+        );
     }
-    
+
+    /**
+     * Removes a edge for a single package from the graph between this package
+     * and the given package
+     *
+     * @param afferentOfPackage The package to remove the afferent edges between
+     */
     public void removePackageIsAfferentOf(VertexPackage afferentOfPackage) {
         for (EdgePackageIsAfferentOf afferentOfEdge : getAfferentOfEdges()) {
             if (afferentOfEdge.getAfferentOf().equals(afferentOfPackage)) {
@@ -119,14 +272,11 @@ public abstract class VertexPackage extends AbstractVertexFrame {
             }
         }
     }
-    
-    @Adjacency(label = "belongsTo", direction = Direction.IN)
-    public abstract List<VertexClass> getBelongsToClasses();
-    
-    @Adjacency(label = "isEfferentOf", direction = Direction.IN)
-    public abstract List<VertexClass> getEffertentOfClasses();
-    
-    @Incidence(label = "isEfferentOf", direction = Direction.IN)
-    public abstract List<EdgeIsEfferentOf> getEffertentOfEdges();
+
+    /**
+     * Removes all package is afferent of edges.
+     */
+    @Adjacency(label = "packageIsAfferentOf")
+    public abstract void removePackageIsAfferentOfEdges();
 
 }
